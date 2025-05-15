bd1fe9f29dcab6b91f7ad8e03ab2262c0a74c0ca
bigcrazyfrog <cibinnikita@gmail.com>
2025-05-15 23:32:50 +0500

    Configure static network addresses in Docker Compose

--- run-with-vpn.sh
+++ run-with-vpn.sh
@@ -1,3 +1,11 @@
-docker network create secure-net
+docker network create \
+  --driver=bridge \
+  --subnet=172.18.0.0/16 \
+  secure-net
 
-docker compose -f jenkins/docker-compose.yml -f nexus/docker-compose.yml -f wiki/docker-compose.yml -f docker-compose-network.yml up -d
+docker compose \
+    -f jenkins/docker-compose.yml \
+    -f nexus/docker-compose.yml \
+    -f wiki/docker-compose.yml \
+    -f docker-compose-network.yml \
+    up -d
